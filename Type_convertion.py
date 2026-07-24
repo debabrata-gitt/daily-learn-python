cd03657418a3d5fa8f4bc933b8488dbaecb1ebcc
debabrata-gitt <pandebabrata380@gmail.com>
2026-07-24 11:20:34 +0530

    Type convertion and Boolean working Process

---
+++ Type_convertion.py
@@ -0,0 +1,25 @@
+a =12
+print(type(a))
+#since here a is an integer.
+
+a=12
+a=str(a)
+print(type(a))
+#here the type of the data is string
+#Thus in python we convert data from one type to another data type.
+
+#Boolean Type
+#It only print either True or False
+#There are 7 falsy values .They are following
+a=0
+a=False
+a=0.00
+a={}
+a=[]
+a=()
+print(bool(a))
+#otherwise what we will write it prints True like ,
+a="debabrata"
+print(bool(a))
+a="123"
+print(bool(a))
