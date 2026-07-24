e3d92386a286ab699901ec5630ad3cd3bc3b4663
debabrata-gitt <pandebabrata380@gmail.com>
2026-07-24 12:44:20 +0530

    Comparison Operators And Its uses.

---
+++ Comparison_Operators.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the First Number :"))
+b=int(input("Enter the Second Number :"))
+#There are six kind of comparison operators . They are "==","<",">","<=",">=","!="
+
+#To check the one is equal to the another we use "=="
+print(a==b)
+
+#To check the one is greater than  to the another we use ">"
+print(a>b)
+
+#To check the one  is smaller than  to the another we use "<"
+print(a<b)
+
+#To check the  one is greater than equal to the another we use ">="
+print(a>=b)
+
+#To check the  one is less than equal to the another we use "<="
+print(a<=b)
+
+#To check the  one is not equal to the another we use "!="
+print(a!=b)
