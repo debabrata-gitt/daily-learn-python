a5efe1eee71061908bfea5eabeb6e39cf75ca654
debabrata-gitt <pandebabrata380@gmail.com>
2026-07-24 12:12:36 +0530

    Types Of Arithmetic Operators And Its Uses.

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the first number :"))
+b=int(input("Enter the second number :"))
+
+print("The addition of the two numbers is :",a+b)
+
+print("The substraction of the two numbers is :",a-b)
+
+print("The multiplication of the two numbers is : ",a*b)
+
+print("The division of two numbers is : ",a/b) #float type 
+print("The division of two numbers is : ",a//b) #integer type
+
+#To Perform  to the power operation we use in python ** here it is 
+
+print ("The square of the first number is :",a**2)
+
+print ("The cube of the second number is :",b**3)
+
+#To perform modulo operation on python we use %.Here it is 
+
+print ("The modulo of the first number is :",a%2)
