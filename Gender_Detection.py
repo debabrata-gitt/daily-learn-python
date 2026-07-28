d9c9524e037cc17392b384fd12758b9e3bf1444e
debabrata-gitt <pandebabrata380@gmail.com>
2026-07-29 01:06:24 +0530

    Gender detection

---
+++ Gender_Detection.py
@@ -0,0 +1,8 @@
+gen= input(" Please tell me the gender  in character form (M or F form):")
+
+if gen == "M" or gen == "m":
+     print("GOOD MORNING SIR")
+elif gen=="F" or gen == "f":
+     print("GOOD MORNING MAAM")
+else: 
+     print("unindentified gender")
