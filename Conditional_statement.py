30456f4f48e67b615490c6104f9c863b11881fdf
debabrata-gitt <pandebabrata380@gmail.com>
2026-07-28 23:46:42 +0530

    Conditional Statement And its Uses

---
+++ Conditional_statement.py
@@ -0,0 +1,25 @@
+#If statement 
+a=int(input("enter the number "))
+if a>10:
+    print("i will do the work.")
+
+#If else statement 
+
+a=5
+if a>10:
+    print("I will do the work A")
+else:
+    print("I will do the work B")
+
+#If,elif,else statement
+
+money=int(input("Enter the amount of money"))
+
+if money ==10:
+    print("I wll have a chocobar ice cream ")
+
+elif money==20:
+    print("I wll have a mango dolly icecream") 
+
+else:
+    print ("I will have a cone ice cream ")    
