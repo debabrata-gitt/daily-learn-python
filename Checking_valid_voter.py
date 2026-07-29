ee7fc08b5d4722d55c0c21ca562860c387ccf683
debabrata-gitt <pandebabrata380@gmail.com>
2026-07-29 11:11:47 +0530

    Valid voter checking.

---
+++ Checking_valid_voter.py
@@ -0,0 +1,7 @@
+name=input("Please enter your name here .")
+age=int(input("Please enter your age here ."))
+
+if age >= 18:
+    print(" you are a valid voter.")
+else:
+    print( " you are not a valid voter.")
