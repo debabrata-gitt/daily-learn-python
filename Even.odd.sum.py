308d2a2c1ce91275e07d826608d682dd7d130733
debabrata-gitt <pandebabrata380@gmail.com>
2026-08-09 14:40:56 +0530

    Sum Of Even And Odd Numbers.

---
+++ Even.odd.sum.py
@@ -0,0 +1,9 @@
+n=int(input("please enter your number."))
+even=0
+odd=0
+for i in range (1,n+1):
+    if i %2==0:
+        even=even+i
+    else:
+        odd=odd+i
+print("The Sum Of Your Even And Odd  Numbers Are:",even , odd)
