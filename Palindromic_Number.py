ae27135a08b29dfd2a3a557c5019e16c0109713c
debabrata-gitt <pandebabrata380@gmail.com>
2026-08-10 16:35:25 +0530

    Checking Palindromic Number Step By Step.

---
+++ Palindromic_Number.py
@@ -0,0 +1,11 @@
+a=int(input("Tell Your Number."))
+copy=a
+rev=0
+while a>0:
+    rev=rev*10+a%10
+    a=a//10
+    print(rev)
+    if copy==rev:
+        print("Palindromic Number.")
+    else:
+        print("Its Not A Palindromic Number.")    
