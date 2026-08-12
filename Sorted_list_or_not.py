d9877866919fd8eb3b21f0993af27cf8f1ff327d
debabrata-gitt <pandebabrata380@gmail.com>
2026-08-12 09:37:43 +0530

    Using Loop Checking A List Is Sorted Or Not.

---
+++ Sorted_list_or_not.py
@@ -0,0 +1,10 @@
+l=[10,11,12,13,14,15]
+
+for i in range (len(l)-1):
+    if l[i] < l[i+1]:
+     continue
+    else:
+        print("Your list is not sorted") 
+        break   
+else:
+   print("your list is sorted.")    
