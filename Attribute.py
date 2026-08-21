ac4720495c775d40cdf48e77f1a82e10e8ac21fd
debabrata-gitt <pandebabrata380@gmail.com>
2026-08-21 22:05:30 +0530

    Performing Attributes And Methods.

---
+++ Attribute.py
@@ -0,0 +1,27 @@
+class Animal:
+    name="lion" #class attribute
+
+
+
+def  __init__(self,age):
+    self.age=age  #instant attribute
+
+def show(self ):  #Instance method   
+    print("How are You?")
+
+
+@classmethod
+def hello(cls):
+    print("How Are You Brother")
+
+
+
+@staticmethod
+def static():
+    print("How Are You.")
+
+
+
+
+
+    
