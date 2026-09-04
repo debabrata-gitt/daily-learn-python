b1bf74fc4f5d25ae002ff3d08815476382b9417a
debabrata-gitt <pandebabrata380@gmail.com>
2026-09-04 22:41:28 +0530

    Replace Any Word Fron A String.

---
+++ Replacing.py
@@ -0,0 +1,8 @@
+text = input("Enter a sentence: ")
+
+old_word = input("Enter word to replace: ")
+new_word = input("Enter new word: ")
+
+result = text.replace(old_word, new_word)
+
+print("Updated sentence:", result)
