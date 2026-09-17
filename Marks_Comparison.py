87a40aec236acfd4306dacf250cd3f5e8d11b072
debabrata-gitt <pandebabrata380@gmail.com>
2026-09-17 10:48:15 +0530

    Comparison Of Marks Using Dunder Method.

---
+++ Marks_Comparison.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def __gt__(self, other):
+        return self.marks > other.marks
+
+
+s1 = Student("Rahul", 85)
+s2 = Student("Amit", 75)
+
+if s1 > s2:
+    print(s1.name, "has higher marks")
+else:
+    print(s2.name, "has higher marks")
