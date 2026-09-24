40ea2c667b9003487a8795c1dd2e3b51fd87ba63
debabrata-gitt <pandebabrata380@gmail.com>
2026-09-25 01:41:03 +0530

    Check A Number Is Prime Or Not.

---
+++ Function_Prime.py
@@ -0,0 +1,16 @@
+def prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+n = int(input("Enter number: "))
+
+if prime(n):
+    print("Prime")
+else:
+    print("Not Prime")
